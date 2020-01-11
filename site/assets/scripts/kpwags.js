a7eec03725eae75b2d98c14fb1d530f2dfecdfae
Keith Wagner <keith@kpwags.com>
2020-01-10 20:10:40 -0500

    Added Site Folder to Separate Art

--- site/assets/scripts/kpwags.js
+++ site/assets/scripts/kpwags.js
@@ -11,14 +11,4 @@ $(document).ready(function() {
         var url = $(this).attr('data-url');
         window.location.href = url;
     });
-
-    $('a').each(function() {
-        var href = $(this).prop('href');
-        var text = $(this).text();
-
-        if (href.indexOf('kpwags.com') < 0 && href.indexOf('127.0.0.1') < 0 && href.substring(0, 1) !== '/') {
-            $(this).prop('target', '_blank');
-            $(this).prop('rel', 'noopener');
-        }
-    });
 });
