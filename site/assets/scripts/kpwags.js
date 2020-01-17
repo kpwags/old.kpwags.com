32c2b5556473f54f08d6b96e668faa732aa133b7
Keith Wagner <keith@kpwags.com>
2020-01-17 18:46:41 -0500

    Started work on a dark mode

--- site/assets/scripts/kpwags.js
+++ site/assets/scripts/kpwags.js
@@ -1,14 +1,41 @@
-$(document).ready(function() {
-    $('#menu-link').click(function() {
-        if ($('.mobile-categories ul').css('display') === 'none') {
-            $('.mobile-categories ul').slideDown();
-        } else {
-            $('.mobile-categories ul').slideUp();
-        }
-    });
+window.onload = function(e) {
+    console.log({darkmode: getCookie("kpwagsdarkmode")});
+    if (getCookie("kpwagsdarkmode") === "1") {
+        document.body.classList.add('dark-mode');
+    }
+}
 
-    $('ul.album-list li, .photography .most-recent .image').click(function() {
-        var url = $(this).attr('data-url');
-        window.location.href = url;
-    });
-});
+function toggleDarkMode() {
+        if (document.body.classList.contains('dark-mode')) {
+            setCookie("kpwagsdarkmode", "0", 365);
+        } else {
+            setCookie("kpwagsdarkmode", "1", 365);
+        }
+
+        document.body.classList.toggle("dark-mode");
+    }
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name+'=; Max-Age=-99999999;';
+}
