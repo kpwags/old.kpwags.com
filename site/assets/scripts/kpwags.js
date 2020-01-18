ce8cb77b65287868f05fbe4a18901b776c607b4d
Keith Wagner <keith@kpwags.com>
2020-01-18 15:43:42 -0500

    Completed Dark Mode & Final Tweaks
    Completed dark mode, updated about page and put in final tweaks

--- site/assets/scripts/kpwags.js
+++ site/assets/scripts/kpwags.js
@@ -1,41 +1,43 @@
 window.onload = function(e) {
-    console.log({darkmode: getCookie("kpwagsdarkmode")});
-    if (getCookie("kpwagsdarkmode") === "1") {
+    if (getCookie('kpwagsdarkmode') === '1') {
         document.body.classList.add('dark-mode');
+        document.getElementById('dark-mode-button').innerText = 'Light Mode';
     }
-}
+};
 
 function toggleDarkMode() {
-        if (document.body.classList.contains('dark-mode')) {
-            setCookie("kpwagsdarkmode", "0", 365);
-        } else {
-            setCookie("kpwagsdarkmode", "1", 365);
-        }
-
-        document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains('dark-mode')) {
+        setCookie('kpwagsdarkmode', '0', 365);
+        document.getElementById('dark-mode-button').innerText = 'Dark Mode';
+    } else {
+        setCookie('kpwagsdarkmode', '1', 365);
+        document.getElementById('dark-mode-button').innerText = 'Light Mode';
     }
+
+    document.body.classList.toggle('dark-mode');
+}
 
 function setCookie(name, value, days) {
-    var expires = "";
+    var expires = '';
     if (days) {
         var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = '; expires=' + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.cookie = name + '=' + (value || '') + expires + '; path=/';
 }
 
 function getCookie(name) {
-    var nameEQ = name + "=";
+    var nameEQ = name + '=';
     var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 }
 
 function eraseCookie(name) {
-    document.cookie = name+'=; Max-Age=-99999999;';
+    document.cookie = name + '=; Max-Age=-99999999;';
 }
