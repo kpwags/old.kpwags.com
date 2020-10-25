ac368aecf9c92c24bf0f5fb8fee9a3e0f420c860
Keith Wagner <keith@kpwags.com>
2020-10-25 14:27:55 -0400

    Updated layout a bit & updated current

--- site/assets/scripts/kpwags.js
+++ site/assets/scripts/kpwags.js
@@ -5,43 +5,41 @@ window.onload = function (e) {
 function initializeMode() {
     if (getCookie('kpwagsoverridemode') === 'dark') {
         document.body.classList.add('dark-mode');
+        currentMode = "DARK";
     } else if (getCookie('kpwagsoverridemode') === 'light') {
         document.body.classList.add('light-mode');
+        currentMode = "LIGHT";
     }
 
-    document.getElementById('open-mode-switcher').onclick = function (e) {
+    document.getElementById('toggle-darkmode-button').onclick = function (e) {
         e.preventDefault();
-        openModeSwitcher();
+        toggleMode();
     };
-
-    const buttons = document.querySelectorAll('.mode-switch-button');
-
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = function (e) {
-            e.preventDefault();
-            switchMode(e.target.id.replace('btn-switch-', ''));
-        };
-    }
 }
 
-function openModeSwitcher() {
-    if (isSwitcherOpen()) {
-        closeModeSwitcher();
-        return;
-    }
-
-    document.addEventListener('click', handleClickOutside);
-
+function toggleMode() {
     let currentMode = getCurrentMode();
-    document.getElementById(`switch-${currentMode}`).classList.add('active');
-    document.getElementById('mode-switcher').style.display = 'block';
-}
 
-function closeModeSwitcher() {
-    document.getElementById('mode-switcher').style.display = 'none';
-    document.removeEventListener('click', handleClickOutside);
-}
+    switch (currentMode) {
+        case 'light':
+            switchMode('dark');
+            break;
 
+        case 'dark':
+            switchMode('light');
+            break;
+
+        default:
+            if (document.body.classList.contains("dark-mode")) {
+                // switch to light mode
+                switchMode('light');
+            } else {
+                // switch to dark mode
+                switchMode('dark');
+            }
+            break;
+    }
+}
 function getCurrentMode() {
     if (getCookie('kpwagsoverridemode') === 'dark') {
         return 'dark';
@@ -52,42 +50,20 @@ function getCurrentMode() {
     }
 }
 
-function isSwitcherOpen() {
-    return document.getElementById('mode-switcher').style.display === 'block';
-}
-
 function switchMode(mode) {
-    document.getElementById(`switch-light`).classList.remove('active');
-    document.getElementById(`switch-system`).classList.remove('active');
-    document.getElementById(`switch-dark`).classList.remove('active');
-
     if (mode === 'light') {
         setCookie('kpwagsoverridemode', 'light', 365);
         document.body.classList.remove('dark-mode');
         document.body.classList.add('light-mode');
-
-        document.getElementById(`switch-system`).classList.remove('active');
-        document.getElementById(`switch-dark`).classList.remove('active');
-        document.getElementById(`switch-light`).classList.add('active');
     } else if (mode === 'dark') {
         setCookie('kpwagsoverridemode', 'dark', 365);
         document.body.classList.remove('light-mode');
         document.body.classList.add('dark-mode');
-
-        document.getElementById(`switch-light`).classList.remove('active');
-        document.getElementById(`switch-system`).classList.remove('active');
-        document.getElementById(`switch-dark`).classList.add('active');
     } else {
         eraseCookie('kpwagsoverridemode');
         document.body.classList.remove('light-mode');
         document.body.classList.remove('dark-mode');
-
-        document.getElementById(`switch-light`).classList.remove('active');
-        document.getElementById(`switch-dark`).classList.remove('active');
-        document.getElementById(`switch-system`).classList.add('active');
     }
-
-    closeModeSwitcher();
 }
 
 function setCookie(name, value, days) {
