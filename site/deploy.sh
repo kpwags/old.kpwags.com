30386d5f0087139ffb790ebf140f23bb776ecd3d
Keith Wagner <244694+kpwags@users.noreply.github.com>
2020-12-14 20:35:28 -0500

    Added an echo to the deploy script to remind me of commands needed for WSL2 deployment

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,3 +1,5 @@
+echo "If running on Ubuntu or WSL2, please run command 'sudo update-alternatives --config java' and select Java 8"
+echo ""
 echo "Building blog"
 jekyll build
 echo "Deploying blog to s3"
